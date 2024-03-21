b628cfd48fa341c42c8c981d91055b6487791542
LuckMandarina <lucas.rodriguez.monge@gmail.com>
2024-03-21 09:32:36 -0600

    SEXO

---
+++ SRC/while.js
@@ -0,0 +1,11 @@
+document.querySelector('#header').innerHTML = "estoy leyendo el documento switch, while.js";
+var mensaje;
+
+frutas = ["fresas", "peras", "Manzana", "naranja", "mango", "cereza"]
+
+var i = 5
+while (i>=0){
+    console.log(i+". " +frutas[i])
+    i = i-1
+}
+
